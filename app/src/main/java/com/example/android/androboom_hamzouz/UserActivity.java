783366d7a124417e995aca135a62dad4a89027be
Hamza <hamza.ouledmoussa@gmail.com>
2017-12-20 11:40:55 +0100

    6th commit

--- app/src/main/java/com/example/android/androboom_hamzouz/UserActivity.java
+++ app/src/main/java/com/example/android/androboom_hamzouz/UserActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -14,13 +15,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.lang.reflect.Array;
 import java.util.Arrays;
@@ -42,6 +53,7 @@ public class UserActivity extends AppCompatActivity {
                     +auth.getCurrentUser());
             TextView textView = (TextView) findViewById(R.id.email);
             textView.setText( auth.getCurrentUser().getEmail());
+            downloadImage();
         } else{
             startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
                     .setAvailableProviders(Arrays.asList(
@@ -70,8 +82,6 @@ public class UserActivity extends AppCompatActivity {
                 }
             }
         );
-        /*TextView textView = (TextView) findViewById(R.id.email);
-        textView.setText( auth.getCurrentUser().toString());*/
     }
 
     protected  void onActivityResult(int requestCode,int resultCode,Intent data){
@@ -85,6 +95,7 @@ public class UserActivity extends AppCompatActivity {
                 rep.getEmail());
                 TextView textView = (TextView) findViewById(R.id.email);
                 textView.setText( auth.getCurrentUser().getEmail());
+                downloadImage();
                 return;
             } else{
                 if (rep == null){
@@ -123,6 +134,7 @@ public class UserActivity extends AppCompatActivity {
                     Bitmap finalBitmap = Bitmap.createScaledBitmap(selectedImage, 500,
                             (selectedImage.getHeight()*500) / selectedImage.getWidth(), false);
                     imageView.setImageBitmap(finalBitmap);
+                    uploadImage();
                 }
                 catch (Exception e) {
                     Log.v("AndroBoum", e.getMessage());
@@ -153,4 +165,66 @@ public class UserActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.actions, menu);
         return true;
     }
+
+
+    private StorageReference getCloudStorageReference() {
+// on va chercher l'email de l'utilisateur connecté
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if(auth ==null)
+            return null;
+        String email = auth.getCurrentUser().getEmail();
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+// on crée l'objet dans le sous-dossier de nom l'email
+        StorageReference photoRef = storageRef.child(email +"/photo.jpg");
+        return photoRef;
+    }
+
+    private void downloadImage() {
+        StorageReference photoRef = getCloudStorageReference();
+        if(photoRef ==null) return;
+        ImageView imageView = (ImageView) findViewById(R.id.imageUser);
+// Load the image using Glide
+        Glide.with(this /* context */).using(new FirebaseImageLoader())
+                .load(photoRef)
+                .skipMemoryCache(true)
+                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                .placeholder(R.drawable.ic_person_black_24dp)
+                .into(imageView);
+    }
+
+    private void uploadImage() {
+        StorageReference photoRef = getCloudStorageReference();
+        if(photoRef == null)return;
+// on va chercher les données binaires de l'image de profil
+        ImageView imageView = (ImageView) findViewById(R.id.imageUser);
+        imageView.setDrawingCacheEnabled(true);
+        imageView.buildDrawingCache();
+        Bitmap bitmap = imageView.getDrawingCache();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100, baos);
+        byte[] data = baos.toByteArray();
+// on lance l'upload
+        UploadTask uploadTask = photoRef.putBytes(data);
+        uploadTask.addOnFailureListener(
+            new OnFailureListener() {
+                @Override
+                public void onFailure( @NonNull Exception exception) {
+// si on est là, échec de l'upload
+                }
+                }).addOnSuccessListener(new
+                OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void
+                    onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+    // ok, l'image est uploadée
+    // on fait pop un toast d'information
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                getString(R.string.imageUploaded),
+                                Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                });
+    }
 }
+
